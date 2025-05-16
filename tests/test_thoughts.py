887ab821a5bfcf2d7563675aac72952e581dbe61
Sara Pinna <s.pinna62@studenti.unica.it>
2025-05-16 12:05:58 +0200

    Fallimento rimosso

--- tests/test_thoughts.py
+++ tests/test_thoughts.py
@@ -60,5 +60,3 @@ def test_get_thought_by_id(client):
     assert response.status_code == 200
     assert response.json["username"] == "anna"
 
-def test_fallimento():
-    assert False, "Test fallito"
